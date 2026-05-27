5e73f4aaa4eb2e704c6df72d0fa8b84bdc38ab6f
allofeve <allofeverythingoneaboveall@gmail.com>
2026-05-27 15:37:43 +0700

    emotion animation

---
+++ animation.bat
@@ -0,0 +1,27 @@
+@echo off
+:loop
+echo [*_*]
+timeout 1 > NUL
+cls
+echo [*o*]
+timeout 1 > NUL
+cls
+echo [^^_^^]
+timeout 1 > NUL
+cls
+echo [^^o^^]
+timeout 1 > NUL
+cls
+echo [O_O]
+timeout 1 > NUL
+cls
+echo [O . O]
+timeout 1 > NUL
+cls
+echo [-_-]
+timeout 1 > NUL
+cls
+echo [- . -]
+timeout 1 > NUL
+cls
+goto loop
